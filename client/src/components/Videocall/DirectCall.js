68a38f33018f889aeedb2577f802d10b52205460
elbytes <elnaz.alimardani@gmail.com>
2021-10-13 03:45:17 -0400

    footer

--- client/src/components/Videocall/DirectCall.js
+++ client/src/components/Videocall/DirectCall.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import LocalVideoView from './LocalVideoView'
